79c21defe88eeab89805f33f73109a097021d774
RudeNinja <79577317+RudeNinja@users.noreply.github.com>
2021-04-29 20:11:47 +0530

    Update Cities&Person.c

--- Cities&Person.c
+++ Cities&Person.c
@@ -3,12 +3,29 @@
 #include<stdlib.h>
 #include<stdbool.h>
 
+struct primaryContacts{
+	int personID;
+        int when_they_met;
+	struct primaryContacts* Next;
+};
+
+
+struct secondaryContacts{
+	int personID;
+	int whenTheyMmet;
+	struct secondaryContacts* Next;
+};
+	
+
 
 struct person{
+	int ID;
 	int Location // Current City of the person.
 	int CovidStatus// 0 for healthy, 1 for Covid+ve
 	int QuarantineEndsOn// date on which the quaranrine ends
 	int RiskFactor // 2--> If is a primary contact , 1--> If is a secondary contact , 0-->If none
+        struct primaryContacts* primaryContactList;
+	struct secondaryContacts* secondaryContactList;
 
 
 };
@@ -19,6 +36,7 @@ typedef struct person* person_ptr;
 
 
 struct city{
+	int CityID;
 	int DangerValue;
 	int CovidPosNum;
 	int PrimaryContacts;
@@ -57,6 +75,8 @@ else
 	printf("Current Status = Negative\n");
 	printf("Please obey all the necessary guidelines issued by the Govt. and practice all the safety measures\n");
 }
+	
+
 
 
 printf("You are currently in City %d\n", A.Location);
@@ -65,3 +85,48 @@ printf("You have a risk factor of %d\n", A.RiskFactor);
 
 
 }
+
+void UpdatePrimaryList(person_ptr A , int newContactID , int meet_date)
+{
+
+	struct primaryContacts* NewContact = (struct primaryContacts*)malloc(sizeof(struct primaryContacts));
+
+	NewContact->personID = newContactID;
+	NewContact->when_they_met = meet_date;
+
+	struct primaryContacts* temp = A->primaryContactList;
+
+	while(temp->Next)
+	{
+		temp = temp->Next;
+	}
+
+	temp->Next = NewContact;
+	NewContact->Next = NULL;
+}
+
+
+void UpdateSecondaryList(person_ptr A , int newContactID , int meet_date)
+{
+
+	struct secondaryContacts* NewContact = (struct secondaryContacts*)malloc(sizeof(struct secondaryContacts));
+
+	NewContact->personID = newContactID;
+	NewContact->when_they_met = meet_date;
+
+	struct secondaryContacts* temp = A->secondaryContactList;
+
+	while(temp->Next)
+	{
+		temp = temp->Next;
+	}
+
+	temp->Next = NewContact;
+	NewContact->Next = NULL;
+}
+	
+
+	
+
+
+  
