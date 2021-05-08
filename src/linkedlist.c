6e7b2886756518ae067c8edf0c6c932f4149d057
Yash Mehan <yash.m@research.iiit.ac.in>
2021-05-08 10:56:29 +0530

    modified edges to have members a, b, weight length

--- src/linkedlist.c
+++ src/linkedlist.c
@@ -1,21 +1,21 @@
 #include "../utils/linkedlist.h"
 #include <stdlib.h>
 
-node* createNode(int b, int w)
+node* createNode(int b, int w, int length)
 {
     node* p = (node*) malloc(sizeof(node));
-    
+    p->length = length;
     p->b = b;
     p->weight = w;
     p->next = NULL; 
     return p;
 }
 
-void pushFront(list* lp, int b, int w)
+void pushFront(list* lp, int b, int w, int length)
 {
     if(*(lp) == NULL)
-    {*lp = createNode(b, w); return;}
+    {*lp = createNode(b, w, length); return;}
     node* t = *lp;
-    *lp = createNode(b, w);
+    *lp = createNode(b, w, length);
     (*(lp))->next = t;
 }
--- utils/linkedlist.h
+++ utils/linkedlist.h
@@ -5,12 +5,13 @@ struct node
 {
     int b;
     int weight;
+    int length;
     struct node* next;
 };
 typedef struct node node;
 typedef node* list;
 
 //=================================FUNC PROTOS==========================================
-node* createNode(int, int);
+node* createNode(int, int, int);
 
-void pushFront(list*, int, int);
+void pushFront(list*, int, int, int);
