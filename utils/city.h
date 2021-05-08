c4b0cabc545bfab5b6dd7b50519aa24bf739ebab
Sai-Akash2009Janapati <akash2009janapati@gmail.com>
2021-05-08 12:18:56 +0530

    Added prototypes in city.h

--- utils/city.h
+++ utils/city.h
@@ -1,6 +1,7 @@
 //DEPENDENCIES vector.h, miscstructs.h
 #pragma once
 #include "vector.h"
+#include "person.h"
 
 struct People{
 	int id;
@@ -17,14 +18,31 @@ struct city{
 	float noOfPrimary;     // calculating danger value.
 	float noOfSecondary;
     double safetyValue;   // = 1.0 / double(dangerValue); always DONT NEED THIS
-	struct people * head; // use linked list of ints for list of persons in this city
+	struct People * head; // use linked list of ints for list of persons in this city
 };
 
 typedef struct city city;
 typedef struct city* city_ptr;
 
-//todo
+// This function adds people belongs to a city to the linked list of people in that city
+void AddPeople(city C[],person P[]);
+
+// This function creates Array of struct cities and returns it's pointer
+city_ptr Create_City_Nodes(int no_of_cities);
+// Here all values are initialised to 0
+
+
 //make a  function whenever covid+ve list is released
-//make a function whenever covid +ve list is released
+void UpdateToPos(city C[],person P[],int array_of_positive[],int size);
+// 'C' is array of city structs, 'array_of_positive' is list of positive, 'size' is size of array
+// 'p' is array of person structs
+
+
 //need to make a function which goes through this city's all residents and printout who all are riskFactor ==1 and ==2;
+void Print_P_S(city C[],persion P[], int id_of_city);
+
+// Prints all info of a particular city
+void Status_of_city(city C[],int city_index);
+
 //updateCityStats function which updates city when noew people leave or enter, updates all members of the struct 
+void Update_City(city C[],persion P[], int origin,int destination,int Persion_ID);
