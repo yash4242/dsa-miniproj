2a9d1d2629643e4ee878c403d9a43b0f68f1f704
Sai Akash Janapati <82522996+Sai-Akash2009Janapati@users.noreply.github.com>
2021-05-08 17:54:49 +0530

    Updated city.h

--- utils/city.h
+++ utils/city.h
@@ -3,33 +3,37 @@
 #include "vector.h"
 #include "person.h"
 
+// Used to store id's of People living in a city
 struct People{
-	int id;
+	int id;						
 	struct People * next;
 };
 
 typedef struct People People;
 typedef struct People * People_Ptr;
 
+// Used to store all information Regarding a particular city
 struct city{
-	float dangerValue;     // Danger Value = covidPosNum + (# Primary/5) + (# Secondary/10)
+	float dangerValue;        // Danger Value = covidPosNum + (# Primary/5) + (# Secondary/10)
 	float covidPosNum;
-	float noOfResidents;   // changed data type to float from int, not to conflict when
-	float noOfPrimary;     // calculating danger value.
+	float noOfResidents;      // changed data type to float from int, not to conflict when
+	float noOfPrimary;        // calculating danger value.
 	float noOfSecondary;
-    double safetyValue;   // = 1.0 / double(dangerValue); always DONT NEED THIS
-	struct People * head; // use linked list of ints for list of persons in this city
+    double safetyValue;       // = 1.0 / double(dangerValue); always DONT NEED THIS
+	struct People * head;     // use linked list of ints for list of persons in this city
 };
 
 typedef struct city city;
 typedef struct city* city_ptr;
 
 // This function adds people belongs to a city to the linked list of people in that city
-void AddPeople(city C[],person P[]);
+// initially when the input is given fron user
+void AddPeople(city C[],person P[],int NoOfPeople);
+
 
 // This function creates Array of struct cities and returns it's pointer
-city_ptr Create_City_Nodes(int no_of_cities);
 // Here all values are initialised to 0
+city_ptr Create_City_Nodes(int no_of_cities);
 
 
 //make a  function whenever covid+ve list is released
@@ -38,11 +42,35 @@ void UpdateToPos(city C[],person P[],int array_of_positive[],int size);
 // 'p' is array of person structs
 
 
-//need to make a function which goes through this city's all residents and printout who all are riskFactor ==1 and ==2;
+// This function Prints all primary and secondary contacts in a city
 void Print_P_S(city C[],person P[], int id_of_city);
 
-// Prints all info of a particular city
+
+// This function Prints all covid positive in a city
+void Print_Positives(city C[],person P[],int id_of_city);
+
+
+
+// Prints all stats of a particular city
 void Status_of_city(city C[],int city_index);
 
+
 //updateCityStats function which updates city when noew people leave or enter, updates all members of the struct 
-void Update_City(city C[],person P[], int origin,int destination,int Person_ID);
+void Update_City(city C[],person P[], int origin,int destination,int Persion_ID);
+
+
+// Used to remove a person from a city when he travels and update the 
+// Remaining info in present city
+void Remove_from_city(city_ptr T,person P[],int Person_ID);
+
+// This function returns danger value of that city
+float DangerValue(city_ptr T);
+
+// This function returns Safety Value of the city
+float SafetyValue(city_ptr T);
+
+// Used to Add a person to person to a city when he travels
+// And updates the remaining info in the city of origin
+void Add_to_city(city_ptr T,person P[],int Person_ID);
+
+
