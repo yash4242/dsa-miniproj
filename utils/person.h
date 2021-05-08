8e5f79a486a3616fe85bfc03c73bf648c96e30b2
mayankbhardwaj719 <mayank.bhardwaj@students.iiit.ac.in>
2021-05-08 21:44:06 +0530

    added defintion of CreatePersonArray and some minor changes

--- utils/person.h
+++ utils/person.h
@@ -11,15 +11,20 @@ typedef struct person person;
 typedef struct person* person_ptr;
 typedef person_ptr List;
 
-struct person{
+struct person
+{
+    int id; // identity of a particular person
 	int location; // Current City of the person.
 	int covidStatus;// 0 for healthy, 1 for Covid+ve
 	int quarantineEndsOn;// date on which the quaranrine ends
 	int riskFactor; // 2--> If is a primary contact of a covid +ve , 1--> If is a secondary contact of a covid + , 0-->If none
     pair* contacts; //during initialising person struct, allocate n indices to this array where n is the no of people in total
-    //pair's "first" element has 0/1/2: 1=> secondary contact of;
+    //pair's "first" element denotes the risk facotr of a person q,related to a person P
+    // It has 0/1/2: 
+    // 1=> secondary contact of P,
+    // 2=> Primary contact of P;
     //pairs "second" element has the date of meeting
-    //aray's index means met which person
+    //aray's index means P met which person
 
 
 };
@@ -32,7 +37,7 @@ struct person{
 // prototypes here and func definitions in ../src/person.c
 
 // create an array of person with  their  respective id and sets the pointer to their contacts as NULL
-person_ptr CreatePersonArray(int No_Of_Persons);
+person_ptr CreatePersonArray(int No_Of_Persons,int Station_No);
 
 
 // Updates the array of person when two person meet i.e when a person goes from a city 1 to city 2
@@ -45,6 +50,29 @@ void PrintList(List L);
 
 //in case this person comes out + then a function which informs his primary contacts& secondary contacts;
 //i.e. updates those people's riskFactor
+
+person_ptr CreatePersonArray(int No_Of_Persons,int Station_No)
+{
+    person_ptr PersonArr=(person_ptr)malloc(No_Of_Persons*sizeof(person));
+    assert(PersonArr!=NULL);
+    
+    PersonArr->contacts= malloc(No_Of_Persons*sizeof(pair));
+    assert(PersonArr->contacts!=NULL);
+     // Create Empty Person Array with assigning the destinantion of each person at a station S
+    for (int i = 1; i <=No_Of_Persons; i++)
+    {
+       PersonArr->contacts[i].first= 0;//assign the risk factor of all the person related to P as 0 as initially no one is positive
+       PersonArr->contacts[i].second= 0;//assign the date of meeting of a person q meeting a person P as 0 as  initially no one met anyone
+       PersonArr[i].id = i; // id denoting the index of each person as its identity
+        PersonArr[i].location = Station_No; // each and every person is not in
+        PersonArr[i].covidStatus = 0;// everyone is healthy intially
+        PersonArr[i]. quarantineEndsOn=0 ;//since initially no one is positive therefore no quarantine
+        PersonArr[i].riskFactor = 0;
+    }
+
+    return PersonArr;
+    
+}
 
 
 
