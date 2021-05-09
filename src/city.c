77b0f5ee681eb6e8edfcd9cb965eee68a002ce0d
Yash Mehan <yash.m@research.iiit.ac.in>
2021-05-09 10:00:58 +0530

    completed city, added transfer person func

--- src/city.c
+++ src/city.c
@@ -3,194 +3,206 @@
 #include <stdlib.h>
 
 
-
-city_ptr Create_City_Nodes(int no_of_cities)
+void cityPrimSec(person personArray[], int cityIndex, int noOfPeople)
 {
-    city_ptr C = (city_ptr)malloc(no_of_cities * sizeof(city));
-
-    for(long i = 0;i < no_of_cities;i++)
+    printf("For City %d, these people are primary contacts of a covid +ve person\n", cityIndex);
+    for(int i = 1; i<=noOfPeople; i++)
     {
-       
-        C[i].covidPosNum = 0;         // Initialise all values to 0
-        C[i].dangerValue = 0;
-        C[i].noOfPrimary = 0;
-        C[i].noOfResidents = 0;
-        C[i].noOfSecondary = 0;
-        C[i].safetyValue = 0;
+        if(personArray[i].location== cityIndex && personArray[i].riskFactor == 2)
+        {printf("%d\n", i);}
     }
 
-    return C;
-}
-
-// Only used initially, when we get input from user
-void AddPeople(city C[],person P[],int NoOfPeople)
-{
-    for(long i = 0;i < NoOfPeople;i++)
+    printf("For City %d, these people are secondary contacts of a covid +ve person\n", cityIndex);
+    for(int i = 1; i<=noOfPeople; i++)
     {
-        int j = P[i].location;
-        C[j].noOfResidents++;
-        
+        if(personArray[i].location== cityIndex && personArray[i].riskFactor == 1)
+        {printf("%d\n", i);}
     }
+
 }
 
-// This function is to be used at initial stage only not after travelling of any person
-void UpdatetoPos(city C[],person P[],int array_of_positive[],int size)
+void printPositivesOfCity(person PersonArray[],int id_of_city,int NoOfPeople)
 {
-    for(long i = 0;i < size;i++)
+    printf("Searching for people from city %d who have covid:\n");
+    int flag = 0;
+    for(int i = 1;i <= NoOfPeople;i++)
     {
-        int j = array_of_positive[i];
-        int location = P[j].location;
-
-        C[location].covidPosNum++;
-        
-    }
-}
-void Update_P_S(city C[],person P[],int noOfPeople)
-{
-    for(int i = 0;i < noOfPeople;i++){
-        int j = P[i].location;
-        if(P[i].riskFactor == 2){
-            C[j].noOfPrimary++;
-        }if(P[i].riskFactor == 1){
-            C[j].noOfSecondary++;
+        if(PersonArray[i].location == id_of_city && PersonArray[i].covidStatus == 1)
+        {
+            printf("%d\n",i);
+            flag =1;
         }
     }
+    if(flag == 0)
+    {printf("City is covid free!\n");}
 }
 
-void Print_P_S(city C[],person P[], int id_of_city,int noOfPeople)
-{
-    printf("List of people who are Primary contacts are : \n");
-    for(int i = 0;i < noOfPeople;i++){
-        if(P[i].location == id_of_city && P[i].riskFactor == 2){
-            printf("%d ",i);
-        }
-    }printf("\n");
 
-    printf("List of people who are secondary contacts are : \n");
-
-    for(int i = 0;i < noOfPeople;i++){
-        if(P[i].location == id_of_city && P[i].riskFactor == 1){
-            printf("%d ",i);
-        }
-    }printf("\n");
-}
-
-// Added to header file
-void Status_of_city(city C[],int city_index)
+void printStatusOfCity(city C[],int city_index)
 {
     city temp = C[city_index];
 
-    printf("Total Population of the city %d id %.0f\n",city_index,temp.noOfResidents);
-    printf("Total number of people who are COVID positive are %.0f",temp.covidPosNum);
-    printf("Total number of people who are Primary contacts of covid people are %f\n",temp.noOfPrimary);
-    printf("Total number of people who are secondary contacts are %.0f\n",temp.noOfSecondary);
+    printf("Total Population of city %d is %d\n",city_index,temp.noOfResidents);
+    printf("Total number of people who are COVID positive are %d\n",temp.covidPosNum);
+    printf("Total number of people who are Primary contacts of covid people are %d\n",temp.noOfPrimary);
+    printf("Total number of people who are secondary contacts are %d\n",temp.noOfSecondary);
     printf("The Danger value of the city is %.2f\n",temp.dangerValue);
-    printf("The safety value of this city is %f\n",temp.safetyValue);
-
-
 }
 
-// Added to header file
-void Remove_from_city(city_ptr T,person P[],int Person_ID)
+//removes person from src, updates src city, adds him to dest, updates dest city, and updates contacts lists, and graph src and dest have to be neighbours. if not then use this func in steps
+void transferPerson(int personIndex, int sourceCity, int destCity, person personArray[], city cityArray[], list graph[], int noOfPerson)
 {
-    /*People_Ptr prev = T->head,current;
-
-    if(T->head != NULL)
-        current = T->head->next;
-
-    while(current){
-        if(current->id == Person_ID){
-            prev->next = current->next;
-            current->next = NULL;
-        }
-        prev = current;
-        current  = current->next;
-        
-    }*/
-
-    T->noOfResidents--;
-    if(P[Person_ID].covidStatus == 1)
+    node* trav = graph[sourceCity];
+    while(trav!= NULL)
     {
-        T->covidPosNum--;
-    }if(P[Person_ID].riskFactor == 2)
-    {
-        T->noOfPrimary--;
-    }if(P[Person_ID].riskFactor == 1)
-    {
-        T->noOfSecondary--;
-    }
-    T->dangerValue = DangerValue(T);
-    T->safetyValue = SafetyValue(T);
-    
-}
-
-// Added to header file
-float DangerValue(city_ptr T)
-{
-    float danger = (T->covidPosNum) + (T->noOfPrimary/5) + (T->noOfSecondary/10);
-
-    return danger;
-}
-
-// Added to header file
-float SafetyValue(city_ptr T)
-{
-    float safety = 1/DangerValue(T);
-
-    return safety;
-}
-
-// Added to header file
-void Add_to_city(city_ptr T,person P[],int Person_ID)
-{
-    /*People_Ptr temp = T->head;
-
-    while(temp->next != NULL)
-    {
-       temp =  temp->next;
-    }
-
-    People_Ptr new = (People_Ptr)malloc(sizeof(People));
-
-    temp->next = new;
-    new->id = Person_ID;
-    new->next = NULL;*/
-
-    T->noOfResidents++;
-    if(P[Person_ID].riskFactor == 1){
-        T->noOfSecondary++;
-    }if(P[Person_ID].riskFactor == 2){
-        T->noOfPrimary++;
-    }if(P[Person_ID].covidStatus == 1){
-        T->covidPosNum++;
+        if(trav->b == destCity){break;}
+        trav= trav->next;
     }
     
-    T->dangerValue = DangerValue(T);
-    T->safetyValue = SafetyValue(T);
-}
-
-void Print_Positives(city C[],person P[],int id_of_city,int NoOfPeople)
-{
-    for(int i = 0;i < NoOfPeople;i++)
+    if(trav == NULL)
     {
-        if(P[i].location == id_of_city && P[i].covidStatus == 1)
+        printf("there is no edge bw %d and %d cities, quitting func %d line in city.c file\n",sourceCity, destCity, __LINE__);
+        return;
+    }
+
+    if(personArray[personIndex].location != sourceCity)
+    {
+        printf("person %d doesnt live in city %d, quitting func. line %d in city.c file.", personIndex, sourceCity, __LINE__);
+        return;
+    }
+
+    if(personArray[personIndex].covidStatus ==1)
+    {
+        printf("this person cant travel becuase he is infected, and thus in quarantine line %d in city.c file, quitting func\n", __LINE__);
+        return;
+    }
+
+    //person is preparing to leave
+    cityArray[sourceCity].noOfResidents--;
+    if(personArray[personIndex].riskFactor ==2) cityArray[sourceCity].noOfPrimary--;
+    
+    if(personArray[personIndex].riskFactor == 1) cityArray[sourceCity].noOfSecondary--;
+
+    cityArray[sourceCity].dangerValue = (double) cityArray[sourceCity].covidPosNum + ((double) cityArray[sourceCity].noOfPrimary)/5.0 + ((double) cityArray[sourceCity].noOfSecondary)/10.0;
+    //now the person has left;
+
+    //now the person has arrived
+    cityArray[destCity].noOfResidents++;
+    personArray[personIndex].location = destCity;
+
+    if(personArray[personIndex].riskFactor ==2)  cityArray[destCity].noOfPrimary--;
+    
+    if(personArray[personIndex].riskFactor == 1) cityArray[destCity].noOfSecondary--;
+
+    cityArray[destCity].dangerValue = (double) cityArray[destCity].covidPosNum + ((double) cityArray[destCity].noOfPrimary)/5.0 + ((double) cityArray[destCity].noOfSecondary)/10.0;
+    
+
+    //all the resident of the new city become this new entrant's primary contacts
+    //and all the
+    for(int i = 1; i<=noOfPerson; i++)
+    {
+        if(personArray[i].location != destCity) continue;
+
+        personArray[personIndex].contacts[i].first = 2;
+        personArray[personIndex].contacts[i].second = currentDate;      //new person's primary contact include city ppl
+
+        personArray[i].contacts[personIndex].first = 2;
+        personArray[i].contacts[personIndex].second = currentDate;      //city ppl's primary contact include new person
+
+        //now giving primary contact of residents to new person as secondary
+        for(int j = 1; j<= noOfPerson; j++)
         {
-            printf("%d ",i);
+            if(personArray[i].contacts[j].first==2 && personArray[i].contacts[j].second >= currentDate-14)
+            {
+                if(personArray[personIndex].contacts[j].first !=2 || personArray[personIndex].contacts[j].second <= currentDate-14)
+                {
+                    personArray[personIndex].contacts[j].first =1;
+                    personArray[personIndex].contacts[j].second =currentDate;
+                }
+
+            }
         }
-    }printf("\n");
+
+        //now giving primary contacts of new person to city ppl as secondary contact
+        for(int j = 1; j<=noOfPerson; j++)
+        {
+            if(personArray[personIndex].contacts[j].first==2 && personArray[personIndex].contacts[j].second>= currentDate-14)
+            {
+                if(personArray[i].contacts[j].first != 2 || personArray[personIndex].contacts[j].second <= currentDate -14)
+                {
+                    personArray[i].contacts[j].first = 1;
+                    personArray[i].contacts[j].second = currentDate;
+                }
+            }
+        }
+
+    }
+
+    
+    //now updating graph edge weights
+    //for eg A----->----B edge will have a weight equal to dangervalue of B
+    //and A----<------B will have edge weight equal to dangervalue of A.
+    //both source city and destinationn city have to update now
+
+    //idea: to know which all cities have edges leading to A, 
+    //we check A's neighbours becuase graph has bidirectional edges
+    //put those edges into a vector, then look at the vector and go to those vertex at the list in the graph
+    //then look of A in the linked list via node* trav and then update the weight. 
+    //doing the same for Destination vertex
+
+    vectorInt* vip = createVectorInt(20);
+    node* trav = graph[sourceCity];
+    while(trav!= NULL)
+    {
+        vectorIntPushBack(vip, trav->b);
+        trav = trav->next;
+    }
+    //obtained all edges of a
+
+    for(int i = 0; i < vip->numelems; i++)
+    {
+        node* trav = graph[vip->arr[i]];
+        while(trav!= NULL)
+        {
+            if(trav->b == sourceCity){break;}
+            trav = trav->next;
+        }
+        if(trav == NULL)
+        {
+            printf("error on line %d of city.c, node from %d to %d exists, but not in reverse? function quitting iteration i = %d\n", __LINE__, sourceCity, vip->arr[i], i);
+            return;
+        }
+        trav->weight = cityArray[sourceCity].dangerValue;
+    }
+    //now updated all edges leading to sourcecity
+
+    //now updating weights of edges leading into destCity
+    vectorInt* vip2 = createVectorInt(20);
+    node* trav = graph[destCity];
+    while(trav!= NULL)
+    {
+        vectorIntPushBack(vip2, trav->b);
+        trav = trav->next;
+    }
+    //obtained all edges of a
+
+    for(int i = 0; i < vip2->numelems; i++)
+    {
+        node* trav = graph[vip2->arr[i]];
+        while(trav!= NULL)
+        {
+            if(trav->b == destCity){break;}
+            trav = trav->next;
+        }
+        if(trav == NULL)
+        {
+            printf("error on line %d of city.c, node from %d to %d exists, but not in reverse? function quitting iteration i = %d\n", __LINE__, destCity, vip2->arr[i], i);
+            return;
+        }
+        trav->weight = cityArray[destCity].dangerValue;
+    }
+    //now updated all edges leading to destCity  
+    
+
 }
 
-void Update_City(city C[],person P[], int origin,int destination,int Person_ID)
-{
-    person temp = P[Person_ID];
-
-    city T1 = C[origin];
-    city T2 = C[destination];
-
-    Remove_from_city(&T1,P,Person_ID);
-    Add_to_city(&T2,P,Person_ID);
-
-    P[Person_ID].location = destination;
-}
-
-// See header file city.h to see the usage of a function
--- utils/city.h
+++ utils/city.h
@@ -3,75 +3,33 @@
 #include "vector.h"
 #include "person.h"
 
-/* Used to store id's of People living in a city
-struct People{
-	int id;						
-	struct People * next;
-};
-*/
 
-typedef struct People People;
-typedef struct People * People_Ptr;
-
-// Used to store all information Regarding a particular city
+//Stores all information Regarding a particular city
 struct city{
-	float dangerValue;        // Danger Value = covidPosNum + (# Primary/5) + (# Secondary/10)
-	float covidPosNum;
-	float noOfResidents;      // changed data type to float from int, not to conflict when
-	float noOfPrimary;        // calculating danger value.
-	float noOfSecondary;
-    double safetyValue;       // = 1.0 / double(dangerValue); always DONT NEED THIS
-//	struct People * head;     // use linked list of ints for list of persons in this city
+	double dangerValue;        // Danger Value = covidPosNum + (# Primary/5) + (# Secondary/10)
+	int covidPosNum;
+	int noOfResidents;      // changed data type to float from int, not to conflict when
+	int noOfPrimary;        // calculating danger value.
+	int noOfSecondary;
+    // double safetyValue;       // = 1.0 / double(dangerValue); always DONT NEED THIS
 };
 
 typedef struct city city;
 typedef struct city* city_ptr;
 
-// This function adds people belongs to a city to the linked list of people in that city
-// initially when the input is given fron user
-void AddPeople(city C[],person P[],int NoOfPeople);
-
-
-// This function creates Array of struct cities and returns it's pointer
-// Here all values are initialised to 0
-city_ptr Create_City_Nodes(int no_of_cities);
-
-
-//make a  function whenever covid+ve list is released
-void UpdateToPos(city C[],person P[],int array_of_positive[],int size);
-// 'C' is array of city structs, 'array_of_positive' is list of positive, 'size' is size of array
-// 'p' is array of person structs
-
 
 // This function Prints all primary and secondary contacts in a city
-void Print_P_S(city C[],person P[], int id_of_city,int noOfPeople);
-
+void cityPrimSec(person personArray[], int cityIndex,int noOfPeople);
 
 // This function Prints all covid positive in a city
-void Print_Positives(city C[],person P[],int id_of_city,int NoOfPeople);
-
+void printPositivesOfCity(person personArray[],int id_of_city,int NoOfPeople);
 
 
 // Prints all stats of a particular city
-void Status_of_city(city C[],int city_index);
+void printStatusOfCity(city C[], int city_index);
 
+//removes person from src, updates src city, adds him to dest, updates dest city, and updates contacts lists, src and dest have to be neighbours. if not then use this func in steps
+void transferPerson(int personIndex, int sourceCity, int destCity, person personArray[], city cityArray[], list graph[], int noOfPerson);
 
-//updateCityStats function which updates city when noew people leave or enter, updates all members of the struct 
-void Update_City(city C[],person P[], int origin,int destination,int Persion_ID);
-
-
-// Used to remove a person from a city when he travels and update the 
-// Remaining info in present city
-void Remove_from_city(city_ptr T,person P[],int Person_ID);
-
-// This function returns danger value of that city
-float DangerValue(city_ptr T);
-
-// This function returns Safety Value of the city
-float SafetyValue(city_ptr T);
-
-// Used to Add a person to person to a city when he travels
-// And updates the remaining info in the city of origin
-void Add_to_city(city_ptr T,person P[],int Person_ID);
 
 
